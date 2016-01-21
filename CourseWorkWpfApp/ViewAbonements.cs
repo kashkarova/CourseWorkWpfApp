4b01db4b3c89a22c5d8387d8d958a7109b71089a
Таня Кашкарова <kashkarovatany@gmail.com>
2016-01-22 01:46:26 +0200

    0

--- CourseWorkWpfApp/ViewAbonements.cs
+++ CourseWorkWpfApp/ViewAbonements.cs
@@ -14,7 +14,6 @@ namespace CourseWorkWpfApp
     
     public partial class ViewAbonements
     {
-        public int id { get; set; }
         public string surname { get; set; }
         public string name { get; set; }
         public string title { get; set; }
